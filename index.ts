522b8fac08fc22e4b017c59d1d5bedeac5541691
xavierbrasher <xavier.brasher@gmail.com>
2021-11-01 21:27:04 +0800

    2.4

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import discordJs, {MessageEmbed, PermissionOverwriteOptions ,PermissionOverwriteManager, PermissionFlags ,BaseCommandInteraction, BaseManager, Guild, GuildBan, GuildBanManager, Intents, Message, Permissions, User, PermissionOverwrites, TextChannel, GuildChannel } from "discord.js"
 import dotenv, { config } from 'dotenv'
 dotenv.config()
-const prefix = "-"
+var prefix = "-"
 const client = new discordJs.Client({
+    //intents of what the bot does
     intents:[
         Intents.FLAGS.GUILD_MESSAGES,
         Intents.FLAGS.GUILDS,
@@ -12,52 +13,60 @@ const client = new discordJs.Client({
 })
 
 client.on("ready", ()=>{
+    //On the bot ready, the bot would log it going online
     console.log("Discord Bot Online 😃🎉")
-    client.user.setActivity('for -help', { type: "WATCHING" })
+    client.user.setActivity('for -help', { type: "WATCHING" }) //sets the activity for the bot
 })
 
 function helpCommand(message:discordJs.Message) {
     let {channel} = message
     const receivedEmbed = message.embeds[0]
+    //Sets the content of the help command
     let content = "You should start with -setup to setup the server and for each channel you would want mute command to work do -setmute\n\n-kick: To kick someone, syntax: -kick @SOMEONE\n\n-ban: To ban someone, syntax: -ban @SOMEONE \n\n-unban: To unban someone (do not use @), syntax: -unban NAME \n\n-play?: To @ people who usually plays, syntax: -play? \n\n-wow: To show how much you appreciate someones message, syntax: -wow \n\n-mute: To mute someone, syntax: -mute @SOMEONE \n\n-unmute: To unmute someone, syntax: -unmute @SOMEONE"
+    //sets up the embed
     const exampleEmbed = new MessageEmbed(receivedEmbed).setTitle("Help").setAuthor("Very Special").setDescription(content).setColor(0x7635cc).setThumbnail("https://cdn.discordapp.com/attachments/847717900286033964/903961400865595443/logo_image_better_Custom.png")
+    //sends it
     channel.send({ embeds: [exampleEmbed] })
 }
 
 function sendCustomEmbedMessage(content:string, title:string, message:discordJs.Message) {
-    let {channel} = message
-    const receivedEmbed = message.embeds[0]
+    let {channel} = message //sets message.channel to channel
+    const receivedEmbed = message.embeds[0] 
+    //Sets the author, title, colour, themnail and content
     const exampleEmbed = new MessageEmbed(receivedEmbed).setTitle(title).setAuthor("Very Special").setDescription(content).setColor(0x7635cc).setThumbnail("https://cdn.discordapp.com/attachments/847717900286033964/903961400865595443/logo_image_better_Custom.png")
+    //Sends the embed in the channel
     channel.send({ embeds: [exampleEmbed] })
 }
 
 function mute(message:discordJs.Message) {
     try {
-        let {mentions, guild, author} = message
-        var MuteRole= guild.roles.cache.find(role => role.name === "MUTE");
-        const target = mentions.users.first()
-        const guildSenderPermissions = guild.members.cache.get(author.id).permissions
+        let {mentions, guild, author} = message //sets mention, guild and author from the message
+        var MuteRole= guild.roles.cache.find(role => role.name === "MUTE") //finds the role in the cache 
+        const target = mentions.users.first() //gets the first user @ in the message
+        const guildSenderPermissions = guild.members.cache.get(author.id).permissions //gets the authors permissions
+        //checks the authors permissions
         if(guildSenderPermissions.has([Permissions.FLAGS.KICK_MEMBERS]) || guildSenderPermissions.has([Permissions.FLAGS.BAN_MEMBERS]) || guildSenderPermissions.has([Permissions.FLAGS.ADMINISTRATOR]) || author.id == "219021504334135296") {
-            console.log("CAN MUTE")
+            console.log("CAN MUTE") //checks
         }
         else {
-            sendCustomEmbedMessage("You do not have permission to mute", "Mute", message)
-            return console.log("CANNOT MUTE")
+            sendCustomEmbedMessage("You do not have permission to mute", "Mute", message) //sends message of no permission
+            return console.log("CANNOT MUTE") //returns
         }
         if (target) {
+            //checks synatx and if it is a bot
             if (target.bot) return sendCustomEmbedMessage("Failed to mute (Check permissions or Make sure it is right syntax -mute @WHOEVER)", "Mute", message)
-            var targetid = message.guild.members.cache.get(target.id)
-            if (target.id == "219021504334135296") return sendCustomEmbedMessage("THATS DADDY I CANT 😦😩", "Mute", message)
-            if (!(targetid == null)) {
+            var targetid = message.guild.members.cache.get(target.id) //gets the members cache from id
+            if (target.id == "219021504334135296") return sendCustomEmbedMessage("THATS DADDY I CANT 😦😩", "Mute", message) //check if it is xavier
+            if (!(targetid == null)) { //check if someone has been choosen
                 targetid.roles.add(MuteRole)
                 let thingyEmbed = 'Muted <@'+ target.id + ">"
-                sendCustomEmbedMessage(thingyEmbed, "Mute", message)
+                sendCustomEmbedMessage(thingyEmbed, "Mute", message) //sends confirmation message
             }  
         }
     }
     catch {
-        sendCustomEmbedMessage("Failed to mute (Check permissions or Make sure it is right syntax -mute @WHOEVER)", "Mute", message)
-        console.log("tried, failed")
+        sendCustomEmbedMessage("Failed to mute (Check permissions or Make sure it is right syntax -mute @WHOEVER)", "Mute", message) //sends retry message
+        console.log("tried, failed") //failed
     }
     
 }
@@ -254,9 +263,6 @@ function Commands(message:discordJs.Message) {
     else if (messageContent == prefix + "wow") {
         message.channel.send("w.o.w s.o c.o.o.l -<@" + message.author.id + ">")
         message.delete()
-    }
-    else if (messageContent.match( prefix +"prefix")) {
-
     }
 }
 
