a3a7fb9c106ae7c8f10c19cf2ec681a2685a5eac
xavierbrasher <xavier.brasher@gmail.com>
2024-02-16 16:51:48 +0800

    changed some things

--- commands/unservermute.ts
+++ commands/unservermute.ts
@@ -1,20 +1,34 @@
-import sendCustomEmbedMessage from "../components/sendCustomEmbedMessage"
-import {Permissions} from "discord.js"
-import * as discordJs from "discord.js"
+import sendCustomEmbedMessage from "../components/sendCustomEmbedMessage";
+import { Permissions } from "discord.js";
+import * as discordJs from "discord.js";
 
-export default async function unserverMute(message:discordJs.Message) {
-    try {
-        const guildSenderPermissions = message.guild.members.cache.get(message.author.id).permissions //checks permissions
-        if (!(guildSenderPermissions.has(Permissions.FLAGS.MANAGE_CHANNELS) || guildSenderPermissions.has(Permissions.FLAGS.ADMINISTRATOR) || message.author.id == "219021504334135296")) return sendCustomEmbedMessage("Do not have permission to do this", "UnServer Mute", message)
-        let target = message.mentions.members.first() //gets target
-        let voiceChannel = target.voice //check voice channel
-        await voiceChannel.setMute(false) //sets mute to false
-        let customMessage = "<@"+target.id+"> has been server unmuted"  //sets up message
-        sendCustomEmbedMessage(customMessage, "UnServer Mute", message) //sends message
-    }
-    catch {
-        sendCustomEmbedMessage("UnServer mute failed, Must be in a voice channel, syntax: -unServerMute @SOMEONE", "UnServer Mute", message) //catches error
-    }
-    
+export default async function unserverMute(message: discordJs.Message) {
+  try {
+    const guildSenderPermissions = message.guild.members.cache.get(
+      message.author.id
+    ).permissions; //checks permissions
+    if (
+      !(
+        guildSenderPermissions.has(Permissions.FLAGS.MANAGE_CHANNELS) ||
+        guildSenderPermissions.has(Permissions.FLAGS.ADMINISTRATOR) ||
+        message.author.id == "438670084307812352"
+      )
+    )
+      return sendCustomEmbedMessage(
+        "Do not have permission to do this",
+        "UnServer Mute",
+        message
+      );
+    let target = message.mentions.members.first(); //gets target
+    let voiceChannel = target.voice; //check voice channel
+    await voiceChannel.setMute(false); //sets mute to false
+    let customMessage = "<@" + target.id + "> has been server unmuted"; //sets up message
+    sendCustomEmbedMessage(customMessage, "UnServer Mute", message); //sends message
+  } catch {
+    sendCustomEmbedMessage(
+      "UnServer mute failed, Must be in a voice channel, syntax: -unServerMute @SOMEONE",
+      "UnServer Mute",
+      message
+    ); //catches error
+  }
 }
-
--- index.ts
+++ index.ts
@@ -10,45 +10,46 @@ const port = 8080;
 dotenv.config();
 
 export default function start() {
-    const server = express();
+  const server = express();
 
-    server.all("*", (req, res) => {
-        res.send("This is just a digital ocean check :)");
-    });
+  server.all("*", (req, res) => {
+    res.send("This is just a digital ocean check :)");
+  });
 
-    server.listen(port, (err) => {
-        if (err) throw err;
-    });
+  server.listen(port, (err) => {
+    if (err) throw err;
+  });
 }
 
 const client = new discordJs.Client({
-    //intents of what the bot does
-    intents: [
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_BANS,
-        Intents.FLAGS.DIRECT_MESSAGES,
-    ],
+  //intents of what the bot does
+  intents: [
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_BANS,
+    Intents.FLAGS.DIRECT_MESSAGES,
+  ],
 });
 
 client.on("ready", () => {
-    if (process.env.PUSH == "1") {
-        sendPush(
-            process.env.PUSHUSER,
-            process.env.PUSHTOKEN,
-            "Discord server is now online"
-        );
-    }
-    //On the bot ready, the bot would log it going online
-    start();
-    console.log("Discord Bot Online 😃🎉");
-    client.user.setActivity("for -help", { type: "WATCHING" }); //sets the activity for the bot
+  if (process.env.PUSH == "1") {
+    sendPush(
+      process.env.PUSHUSER,
+      process.env.PUSHTOKEN,
+      "Discord server is now online"
+    );
+  }
+  //On the bot ready, the bot would log it going online
+  start();
+  console.log("Discord Bot Online 😃🎉");
+  client.user.setActivity("for -help", { type: "WATCHING" }); //sets the activity for the bot
 });
 
 client.on("messageCreate", (message) => {
-    //console.log("Server: "+message.guild.name+ ", Channel: " +message.channel + ", Username: " + message.author.username+", Message: " + message.content); //logs the message
+  //console.log("Server: "+message.guild.name+ ", Channel: " +message.channel + ", Username: " + message.author.username+", Message: " + message.content); //logs the message
+  if (message.author.bot) return;
 
-    if (message.content[0] == "-") Commands(message, client); //if the first letter = to prefix than run commands
+  if (message.content[0] == "-") Commands(message, client); //if the first letter = to prefix than run commands
 });
 
 client.login(process.env.TOKEN); //logs in with the token
